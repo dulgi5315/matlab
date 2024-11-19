6fb800a3fdd08b75d7cf7ec7e2fce712b74d0684
dulgi5315 <154944333+dulgi5315@users.noreply.github.com>
2024-11-19 10:57:34 +0900

    error code

--- error.py
+++ error.py
@@ -1,1 +1,50 @@
-AttributeError: 'QLabel' object has no attribute 'setTransform'AttributeError: 'QLabel' object has no attribute 'setTransform'
+QSocketNotifier: Can only be used with threads started with QThread
+qt.qpa.wayland: Wayland does not support QWindow::requestActivate()
+QWidget::paintEngine: Should no longer be called
+QPainter::begin: Paint device returned engine == 0, type: 1
+QPainter::setFont: Painter not active
+QPainter::translate: Painter not active
+QPainter::rotate: Painter not active
+QPainter::end: Painter not active, aborted
+QWidget::paintEngine: Should no longer be called
+QPainter::begin: Paint device returned engine == 0, type: 1
+QPainter::setFont: Painter not active
+QPainter::translate: Painter not active
+QPainter::rotate: Painter not active
+QPainter::end: Painter not active, aborted
+QWidget::paintEngine: Should no longer be called
+QPainter::begin: Paint device returned engine == 0, type: 1
+QPainter::setFont: Painter not active
+QPainter::translate: Painter not active
+QPainter::rotate: Painter not active
+QPainter::end: Painter not active, aborted
+QWidget::paintEngine: Should no longer be called
+QPainter::begin: Paint device returned engine == 0, type: 1
+QPainter::setFont: Painter not active
+QPainter::translate: Painter not active
+QPainter::rotate: Painter not active
+QPainter::end: Painter not active, aborted
+QWidget::paintEngine: Should no longer be called
+QPainter::begin: Paint device returned engine == 0, type: 1
+QPainter::setFont: Painter not active
+QPainter::translate: Painter not active
+QPainter::rotate: Painter not active
+QPainter::end: Painter not active, aborted
+QWidget::paintEngine: Should no longer be called
+QPainter::begin: Paint device returned engine == 0, type: 1
+QPainter::setFont: Painter not active
+QPainter::translate: Painter not active
+QPainter::rotate: Painter not active
+QPainter::end: Painter not active, aborted
+QWidget::paintEngine: Should no longer be called
+QPainter::begin: Paint device returned engine == 0, type: 1
+QPainter::setFont: Painter not active
+QPainter::translate: Painter not active
+QPainter::rotate: Painter not active
+QPainter::end: Painter not active, aborted
+QWidget::paintEngine: Should no longer be called
+QPainter::begin: Paint device returned engine == 0, type: 1
+QPainter::setFont: Painter not active
+QPainter::translate: Painter not active
+QPainter::rotate: Painter not active
+QPainter::end: Painter not active, aborted
